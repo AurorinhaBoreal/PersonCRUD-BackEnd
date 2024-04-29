e7c83005b1cce390a04381971530a9245f7396a9
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-04-29 17:42:10 -0300

    feat: Controllers redirecting to Repositories

--- src/main/java/com/db/crud/person/controller/AddressController.java
+++ src/main/java/com/db/crud/person/controller/AddressController.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.db.crud.person.dto.AddressDTO;
 import com.db.crud.person.entity.Address;
+import com.db.crud.person.repository.AddressRepository;
+import com.db.crud.person.repository.PersonRepository;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +20,11 @@ import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/address")
-public class AddressController {
-    
+public class AddressController {    
     Logger logger = Logger.getLogger(AddressController.class.getName());
-    Address address;
+
+    @Autowired // Injeção de Dependências - Injetando a Person Repository na Controller
+    private AddressRepository repository;
     
     @GetMapping("/list")
     public void listAddress() {
@@ -29,6 +33,7 @@ public class AddressController {
 
     @PostMapping("/create")
     public void createUser(@RequestBody @Valid AddressDTO address) {
+        repository.save(new Address(address));
         logger.log(Level.INFO, "O Corpo do endereço: \n"+address);
     }
     
--- src/main/java/com/db/crud/person/controller/PersonController.java
+++ src/main/java/com/db/crud/person/controller/PersonController.java
@@ -3,10 +3,13 @@ package com.db.crud.person.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.db.crud.person.dto.PersonDTO;
+import com.db.crud.person.entity.Person;
+import com.db.crud.person.repository.PersonRepository;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +23,9 @@ import java.util.logging.Logger;
 @RequestMapping("/person")
 public class PersonController {
     Logger logger = Logger.getLogger(PersonController.class.getName());
+
+    @Autowired // Injeção de Dependências - Injetando a Person Repository na Controller
+    private PersonRepository repository;
     
     @GetMapping("/list")
     public void listPersons() {
@@ -28,6 +34,7 @@ public class PersonController {
 
     @PostMapping("/create")
     public void createUser(@RequestBody @Valid PersonDTO person) {
+        repository.save(new Person(person));
         logger.log(Level.INFO, "O Corpo da pessoa: \n"+person);
     }
 }
