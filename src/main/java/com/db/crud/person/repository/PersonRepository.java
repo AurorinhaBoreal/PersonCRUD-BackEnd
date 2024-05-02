0779547c82a9b0b26cd063ab05f02e1c6d35c424
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-02 12:34:31 -0300

    feat: Implements Customized Method

--- src/main/java/com/db/crud/person/repository/PersonRepository.java
+++ src/main/java/com/db/crud/person/repository/PersonRepository.java
@@ -3,8 +3,10 @@ package com.db.crud.person.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.db.crud.person.entity.Person;
+import java.util.List;
+
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     
-    
+    boolean existsByCpf(String cpf);
 }
