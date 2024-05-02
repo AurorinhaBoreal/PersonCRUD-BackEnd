62428778eb0d120c4c65b28fdeeae1fd15ea43a8
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-02 12:31:02 -0300

    feat: Create Customized Expections CreateAddress and CreatePerson

---
+++ src/main/java/com/db/crud/person/exception/CreatePersonException.java
@@ -0,0 +1,7 @@
+package com.db.crud.person.exception;
+
+public class CreatePersonException extends RuntimeException {
+    public CreatePersonException(String message) {
+      super(message);
+    }
+  }
