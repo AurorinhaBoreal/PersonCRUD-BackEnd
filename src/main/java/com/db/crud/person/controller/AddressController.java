142804f4e79942192f1d7b5ecc6458ccd1f09e99
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-03 12:45:40 -0300

    feat: Implements Create Address and List Address

--- src/main/java/com/db/crud/person/controller/AddressController.java
+++ src/main/java/com/db/crud/person/controller/AddressController.java
@@ -11,8 +11,12 @@ import com.db.crud.person.service.AddressService;
 import com.db.crud.person.service.PersonService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,30 +29,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/address")
+@Slf4j
 public class AddressController {    
     Logger logger = Logger.getLogger(AddressController.class.getName());
 
-    @Autowired // Injeção de Dependências - Injetando a Person Repository na Controller
-    private AddressRepository repoA;
-
     @Autowired
-    private AddressService servA;
-
-    @Autowired
-    private PersonRepository repoP;
+    private AddressService service;
     
     @GetMapping("/list")
-    public List<Address> listAddress() {
-        return servA.list();
+    public ResponseEntity<List<Address>> listAddress() {
+        var body = service.list();
+
+        return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
     // TODO: Fix insertion of Address in Database - Assign Address to Person
-    // @PostMapping("/{personID}/assign")
-    // public void createAddress(@RequestBody @Valid AddressDTO address, @PathVariable Long personID) {
-    //     Person person = repoP.findById(personID).get();
-    //     repoA.save(new Address(address));
-    //     logger.log(Level.INFO, "O Corpo do endereço: \n"+address);
-    //     person.
-    // }
+    @PostMapping("/{personID}/assign")
+    public ResponseEntity<Address> createAddress(@RequestBody @Valid AddressDTO address, @PathVariable Long personID) {
+        // System.out.println("O DTO do Address: "+address);
+
+        System.out.println(address.isMainAddress());
+
+        var body = service.create(address, personID);
+
+        logger.log(Level.INFO, "O Corpo do endereço: \n"+address);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+        
+    }
     
 }
