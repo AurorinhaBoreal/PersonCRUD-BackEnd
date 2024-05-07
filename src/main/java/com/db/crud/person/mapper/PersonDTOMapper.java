a9f989b445d835ed674272763bc9e98676464068
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-07 13:08:12 -0300

    feat: Add Mapper Files

--- src/main/java/com/db/crud/person/mapper/PersonDTOMapper.java
+++ src/main/java/com/db/crud/person/mapper/PersonDTOMapper.java
@@ -1,5 +1,20 @@
 package com.db.crud.person.mapper;
 
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.db.crud.person.dto.PersonDTO;
+import com.db.crud.person.entity.Person;
+
 public interface PersonDTOMapper {
-    
+
+    // Cria uma instância do Mapper
+    PersonDTOMapper INSTANCE = Mappers.getMapper(PersonDTOMapper.class);
+
+    // Converte de Entidade para DTO
+    @Mapping(target = "id", ignore = true)
+    PersonDTO EntityToDTO(Person person);
+
+    // Converte de DTO para Entidade
+    Person DTOToEntity(PersonDTO personDTO);
 }
