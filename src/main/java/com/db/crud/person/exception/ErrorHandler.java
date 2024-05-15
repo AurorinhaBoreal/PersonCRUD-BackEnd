8b2fa44252cb123d765f2847d9caa21015b20319
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-15 12:42:20 -0300

    feat: Implements Error Handlers

---
+++ src/main/java/com/db/crud/person/exception/ErrorHandler.java
@@ -0,0 +1,47 @@
+package com.db.crud.person.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ErrorHandler {
+    @ExceptionHandler(CreateAddressException.class)
+    public ResponseEntity<String> handleErrorCreateAddressException(CreateAddressException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CreatePersonException.class)
+    public ResponseEntity<String> handleErrorCreatePersonException(CreatePersonException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UpdateAddressException.class)
+    public ResponseEntity<String> handleErrorUpdateAddressException(UpdateAddressException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UpdatePersonException.class)
+    public ResponseEntity<String> handleErrorUpdatePersonException(UpdatePersonException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DeleteAddressException.class)
+    public ResponseEntity<String> handleErrorDeleteAddressException(DeleteAddressException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+
+    @ExceptionHandler(DeletePersonException.class)
+    public ResponseEntity<String> handleErrorDeletePersonException(DeletePersonException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+    
+    
+    @ExceptionHandler(GetInfoException.class)
+    public ResponseEntity<String> handleErrorGetInfoException(GetInfoException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+    
+}
