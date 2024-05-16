d5a78ff1cd6e04f240398f67565ea376d771b3e1
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-16 09:00:22 -0300

    feat: Implements ErrorHandler

--- src/main/java/com/db/crud/person/exception/ErrorHandler.java
+++ src/main/java/com/db/crud/person/exception/ErrorHandler.java
@@ -2,23 +2,14 @@ package com.db.crud.person.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler(CreateAddressException.class)
-    public ResponseEntity<String> handleErrorCreateAddressException(CreateAddressException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
     @ExceptionHandler(CreatePersonException.class)
-    public ResponseEntity<String> handleErrorCreatePersonException(CreatePersonException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
-    @ExceptionHandler(UpdateAddressException.class)
-    public ResponseEntity<String> handleErrorUpdateAddressException(UpdateAddressException e) {
+    public ResponseEntity<String> handleErrorCreatePersoExceptionn(CreatePersonException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
@@ -27,20 +18,33 @@ public class ErrorHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
-    @ExceptionHandler(DeleteAddressException.class)
-    public ResponseEntity<String> handleErrorDeleteAddressException(DeleteAddressException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
-
-
     @ExceptionHandler(DeletePersonException.class)
     public ResponseEntity<String> handleErrorDeletePersonException(DeletePersonException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(CreateAddressException.class)
+    public ResponseEntity<String> handleErrorCreateAddressException(CreateAddressException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
-    
+
+    @ExceptionHandler(UpdateAddressException.class)
+    public ResponseEntity<String> handleErrorUpdateAddressException(UpdateAddressException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DeleteAddressException.class)
+    public ResponseEntity<String> handleErrorDeleteAddressException(DeleteAddressException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }    
     
     @ExceptionHandler(GetInfoException.class)
     public ResponseEntity<String> handleErrorGetInfoException(GetInfoException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleErrorMethodArgumentoNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
     
