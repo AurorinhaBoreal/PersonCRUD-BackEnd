8f833ca4355fc44ef6f5ee816ff2b671183a72aa
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-16 08:58:31 -0300

    refactor: Change PersonDTO to a Record

--- src/main/java/com/db/crud/person/dto/PersonDTO.java
+++ src/main/java/com/db/crud/person/dto/PersonDTO.java
@@ -5,27 +5,25 @@ import java.time.LocalDate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PersonDTO {
-
+// TODO: FIX CODE BECAUSE OF RECORD!!
+@Builder
+public record PersonDTO( 
     @NotBlank(message = "Primeiro nome não pode ser vazio")
-    private String firstName;
+    String firstName,
 
     @NotBlank(message = "Último nome não pode ser vazio")
-    private String lastName;
+    String lastName,
 
     @NotBlank(message = "Informe um CPF!")
     @CPF(message = "CPF Inválido!")
-    private String cpf;
+    String cpf,
 
     @NotNull(message = "Informe uma data válida!")
-    private LocalDate birthDate;
+    LocalDate birthDate) {
+
+   ;
 
     @Override
     public String toString() {
