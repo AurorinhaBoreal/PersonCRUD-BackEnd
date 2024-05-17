9a140a694720cd4cb72ae427399b3806b89d9941
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-05-17 11:23:33 -0300

    refactor: Remove Comments

--- src/main/java/com/db/crud/person/PersonApplication.java
+++ src/main/java/com/db/crud/person/PersonApplication.java
@@ -3,7 +3,7 @@ package com.db.crud.person;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// TODO: FIX CODE BECAUSE OF RECORD!!
+
 @SpringBootApplication
 public class PersonApplication {
 
