5a25020e3f9127d80926ab346c3aeb4f5893ae8b
Aurora Kruschewsky <364-aurora.rossi@users.noreply.gitlab.dbserver.com.br>
2024-07-05 13:40:39 -0300

    Revert "docs: Delete a Space"
    This reverts commit 96d283713b52c30f7bbfd8e5f984e9aaa28d18d6.

--- src/main/java/com/db/crud/person/entity/Person.java
+++ src/main/java/com/db/crud/person/entity/Person.java
@@ -17,7 +17,8 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
